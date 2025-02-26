5b32abec8806357cd339be1a3ec0672146f3084b
Hamid-dev13 <hamid.bennacef@epitech.eu>
2025-02-26 09:46:39 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const projects = [
   {
     title: 'FreelanceFlow',
     category: 'DÉVELOPPEMENT FULL STACK • REACT / NODE.JS',
-    description: 'Application weApplication web pensée pour les freelances, permettant de stocker les clients et les différentes missions associées à ces derniers. Authentification utilisateur, interface dynamique, dashboard, communication en temps réel avec une base de données PostgreSQL.sb pour les freelances, gestion de clients et missions, interface dynamique avec base de données MySQL.',
+    description: 'Application web pensée pour les freelances, permettant de stocker les clients et les différentes missions associées à ces derniers. Authentification utilisateur, interface dynamique, dashboard, communication en temps réel avec une base de données PostgreSQL.sb pour les freelances, gestion de clients et missions, interface dynamique avec base de données MySQL.',
     image: 'https://images.unsplash.com/photo-1460472178825-e5240623afd5?w=500&h=300&fit=crop',
     tags: ['React', 'Nextjs', 'MySQL'],
     link: 'https://freelance-flow-theta.vercel.app/'
@@ -27,7 +27,7 @@ const projects = [
   {
     title: 'Internet Relay Chat',
     category: 'DÉVELOPPEMENT BACKEND • TYPESCRIPT / WEBSOCKET',
-    description: 'Application web de chat instanApplication web de chat instantané, dynamique, permettant à plusieurs utilisateurs de communiquer en temps réel. Solution complète intégrant un front-end moderne et un back-end robuste. Implémentation de fonctionnalités avancées telles que la messagerie privée, les notifications et la gestion des utilisateurs.tané, dynamique, permettant à plusieurs utilisateurs de communiquer en temps réel.',
+    description: 'Application web de chat instantané, dynamique, permettant à plusieurs utilisateurs de communiquer en temps réel. Solution complète intégrant un front-end moderne et un back-end robuste. Implémentation de fonctionnalités avancées telles que la messagerie privée, les notifications et la gestion des utilisateurs.tané, dynamique, permettant à plusieurs utilisateurs de communiquer en temps réel.',
     image: 'https://images.unsplash.com/photo-1523240795612-9a054b0db644?w=500&h=300&fit=crop',
     tags: ['React', 'Node.js', 'WebSocket'],
     link: '#'
